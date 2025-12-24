7ec3a58e738e7ed1f812f53464aa6afc2d47da2d
Leo Willems <leo@lewi.nl>
2025-12-24 15:31:05 +0100

    initial code

---
+++ RecSnd/RecSnd.ino
@@ -0,0 +1,91 @@
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+#include <printf.h>
+
+#define PICO
+
+#if defined(PICO)
+  #define CE_PIN  14
+  #define CSN_PIN 15
+  #define BUTTON_PIN 16
+#else
+  #define CE_PIN  9
+  #define CSN_PIN 8
+  #define BUTTON_PIN 4
+#endif
+
+RF24 radio(CE_PIN, CSN_PIN); // CE, CSN
+
+const uint64_t commonAddress = 0xE8E8F0F0E1LL;
+
+#define NODE_ID 1  // Set to 1, 2, or 3
+#define MSGLEN 28
+struct Payload {
+  int nodeID;
+  char message[MSGLEN];
+};
+
+void setup() {
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  while (!Serial) {
+        delay(10); 
+    }
+  printf_begin();
+
+  delay(500);
+
+#ifdef PICO
+  SPI.setSCK(6);
+  SPI.setTX(7);
+  SPI.setRX(4);
+  SPI.begin();
+#endif
+
+  radio.begin();
+
+  if (!radio.begin()) {
+    Serial.println("nRF24L01 Hardware not found!");
+    while (1); // Halt
+  }
+
+  //radio.printDetails();
+  radio.openWritingPipe(commonAddress);
+  radio.openReadingPipe(1, commonAddress);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setAutoAck(false);
+  radio.startListening();
+};
+
+void loop() {  
+
+
+  if (digitalRead(BUTTON_PIN) == 0 ) {
+  
+    Payload data;
+    data.nodeID = NODE_ID;
+    String h = "Hallo";
+    h.toCharArray(data.message, MSGLEN);
+    radio.stopListening();
+    for(int i=0; i<3; i++) {
+      radio.write(&data, sizeof(data));
+      delay(100);
+    }
+    radio.startListening();
+    return;
+  }
+  
+  if (radio.available()) {
+    char txt_received[12] = "";
+    radio.read(&txt_received, sizeof(txt_received)); 
+    Serial.print("Received from: ");Serial.println(txt_received);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    //radio.stopListening(); 
+  }
+}
